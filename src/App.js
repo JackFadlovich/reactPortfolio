ba12162f7d969af7c9493a91ace93c4b0ebba1cf
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-08-24 15:58:00 -0500

    exported files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+import Navigation from './components/navigation';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Contact from './components/contact';
+import Resume from './components/resume';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
