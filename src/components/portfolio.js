59bc20985d76407f7a13f9d627ac97e9762328bb
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-08-29 09:33:40 -0500

    portfolio project CSS done and one project is displayed

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Footer from './Footer';
 function Portfolio() {
     return (
-        <div>
-            <h1>Portfolio</h1>
-            <p>
-                idk ask ur mother
-            </p>
+        <div className="portfolio-section">
+        <h1>My Projects</h1>
+        <div className="portfolio-item">
+            <a href="https://notetaker-yanp.onrender.com/" target="_blank" rel="noopener noreferrer">
+                <img src={`${process.env.PUBLIC_URL}/Images/Note-Taker-SS.png`} alt="Note Taker" />
+            </a>
+            <p>Note Taker</p>
         </div>
-    )
+    </div>
+)
 }
 
 <Footer />
