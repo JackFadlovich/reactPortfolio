cf6e89c39b5dc1e470edc8c8d20095bec87e300b
Jack <108418371+ZolarEclipse@users.noreply.github.com>
2024-09-12 10:59:50 -0500

    test didnt work

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import "../styles/App.css";
-import "../styles/about.css";
+import "../styles/About.css";
 
 function About() {
     return (
