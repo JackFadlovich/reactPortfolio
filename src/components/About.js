eb70a2bf589718e0bb2367bd733162b5a2258aec
JackFadlovich <JAFadlovich@gmail.com>
2025-03-04 13:49:31 -0600

    updated year

--- src/components/About.js
+++ src/components/About.js
@@ -14,7 +14,7 @@ function About() {
                 <p>
                     
 
-                I am a web developer with 3 years of coding experience, specializing in JavaScript and React. I recently completed a Web Development bootcamp at Washington University, where I honed my skills in building responsive and interactive web applications. Throughout my internship, I worked on a collaborative team to develop a web application, gaining valuable experience in both front-end and back-end development with technologies like React, Node.js, and MongoDB.
+                I am a web developer with 4 years of coding experience, specializing in JavaScript and React. I recently completed a Web Development bootcamp at Washington University, where I honed my skills in building responsive and interactive web applications. Throughout my internship, I worked on a collaborative team to develop a web application, gaining valuable experience in both front-end and back-end development with technologies like React, Node.js, and MongoDB.
 
 I thrive in team environments and enjoy the process of collaborating with others to bring ideas to life. As I continue to grow in my career, I'm excited to apply my skills and contribute to projects that challenge me to think critically and creatively.
 </p>
